c40c9f4eb59e5f2c93bad3a6ffcd6321c27d2639
wuqi <975460660@qq.com>
2021-03-31 11:25:33 +0800

    Init

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,19 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential"],
   parserOptions: {
     parser: "babel-eslint",
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-undef': 'off',
+    'generator-star-spacing': 'off',
+    'space-before-function-paren': 'off',
+    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
+    'eol-last': 'off',
+    camelcase: 'off',
+    quotes: [1, 'single']
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false;
+import 'normalize.css'
+import '@/assets/styles/icon.css'
+import '@/assets/styles/global.scss'
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
