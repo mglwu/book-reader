158f207b2fccadddf44a197e257ca27b0c057d16
wuqi <975460660@qq.com>
2021-03-31 11:25:59 +0800

    Feat titlebar

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <transition name="slide-down">
+    <div v-show="ifTitleAndMenuShow" class="title-wrapper">
+      <div class="left">
+        <span class="icon-back icon"></span>
+      </div>
+      <div class="right">
+        <div class="icon-wrapper">
+          <span class="icon-cart icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-person icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-more icon"></span>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    ifTitleAndMenuShow: {
+      type: Boolean,
+      default: false
+    }
+  },
+  name: 'TitleBar'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/global.scss';
+.title-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 101;
+  display: flex;
+  width: 100%;
+  height: px2rem(48);
+  background-color: white;
+  box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, 0.15);
+  .left {
+    @include center;
+    flex: 0 0 px2rem(60);
+  }
+  .right {
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+    .icon-wrapper {
+      flex: 0 0 px2rem(40);
+      @include center;
+      .icon-cart {
+        font-size: px2rem(22);
+      }
+    }
+  }
+}
+</style>
